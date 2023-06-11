98667307d7d509b7aa5094ac4f2335e1eb48bfb7
sgaabdu4 <abdulgafoorabid@gmail.com>
2023-06-11 18:08:59 +0100

    first commit

---
+++ android/app/src/main/kotlin/com/example/display_current_date_and_time/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.display_current_date_and_time
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+void main() {
+  runApp(const ProviderScope(child: MyApp()));
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+
+//Create a provider that gets the date and time now
+final Provider<DateTime> currentDate = Provider((ref) => DateTime.now());
+
+class MyHomePage extends ConsumerWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    //watch the date value and then adds it once to the text
+    final date = ref.watch(currentDate);
+    return Scaffold(
+      appBar: AppBar(),
+      body: Center(child: Text(date.toString())),
+
+    );
+  }
+}
